ccbfb1be0c59dca38f47eca79f707c6be4cc3baa
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-12 12:46:18 +0200

    changed search endpoint; added simple UI

--- src/main/java/net/schwarzbaer/java/spring/simplerest/PersonRepository.java
+++ src/main/java/net/schwarzbaer/java/spring/simplerest/PersonRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 //@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long>, CrudRepository<Person,Long> {
 
+	@RestResource(path = "lastnames", rel = "lastnames")
 	List<Person> findByLastName(@Param("name") String name);
 
 }
